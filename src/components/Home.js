6a0d80195c1c59dd6523dd483ed2462b493e5287
Zach Lynch <zachary.lynch1337@gmail.com>
2021-07-22 05:43:34 -0700

    trying to get around CORS for api like I did before

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,18 +12,14 @@ export default class Home extends Component {
     // http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=B9D7B8759C0EA46E770FE3F9E3250924&steamids=76561197960435530
 
     // get owned games 
-    // http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=B9D7B8759C0EA46E770FE3F9E3250924&steamid=76561197960434622&format=json
+    // http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=B9D7B8759C0EA46E770FE3F9E3250924&steamid=76561197972495328&format=json
 
     // get friends list
     // http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=440&key=B9D7B8759C0EA46E770FE3F9E3250924&steamid=76561197972495328
     componentDidMount() {
-        fetch(`http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=B9D7B8759C0EA46E770FE3F9E3250924&steamid=76561198036778665&relationship=friend`, {
-            crossDomain: true, 
-            method: 'GET',
-            headers: {'Content-Type': 'application/json'},
-          })
+        fetch(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=B9D7B8759C0EA46E770FE3F9E3250924&steamids=76561197960435530`, {mode: 'no-cors', header: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}})
         .then(res => res.json)
-        .then(res => this.setState({data: res}))
+        .then(res2 => this.setState({data: res2}))
     }
 
     render() {
